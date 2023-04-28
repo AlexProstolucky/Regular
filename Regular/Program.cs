7f222bc67ee8ae018b83d2d030be434a10911e07
AlexProstolucky <alexprostolucky@gmail.com>
2023-04-28 19:06:42 +0300

    Добавьте файлы проекта.

---
+++ Regular/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Input the password to check: ");
+        string checker = @"([a-z\d{3,}]){9}";
+        Regex reg = new Regex(checker);
+        string buf = Console.ReadLine();
+        Console.WriteLine(reg.IsMatch(buf));
+    }
+}
